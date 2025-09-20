6aaa9626aaf76e7eb80cc01cc7f9b4718774f67b
DiadkoPropan <arsenij.guba.2016@gmail.com>
2025-09-20 16:19:21 +0300

    f

---
+++ src/services/movieService.ts
@@ -0,0 +1,21 @@
+import axios from "axios";
+import type { Movie } from "../types/movie";
+
+const API_URL = "https://api.themoviedb.org/3/search/movie";
+const API_TOKEN = import.meta.env.VITE_API_TOKEN;
+
+interface FetchMoviesResponse {
+  results: Movie[];
+}
+
+export const fetchMovies = async (query: string): Promise<Movie[]> => {
+  const config = {
+    params: { query },
+    headers: {
+      Authorization: `Bearer ${API_TOKEN}`,
+    },
+  };
+
+  const response = await axios.get<FetchMoviesResponse>(API_URL, config);
+  return response.data.results;
+};
